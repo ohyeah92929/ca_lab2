6fd1a58702531dc5c28482a3db021b41261747e7
esalcort <32207831+esalcort@users.noreply.github.com>
2017-09-25 13:24:44 -0500

    Added run test script

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 echo "Test starting..." > test.log
 rm results.log
-while read input output expected err_code
+while read asm_file golden in_cmd
 do
-	echo Assembling $input >> test.log
-	../assemble $input $output >> test.log
-	if [ $? != $err_code ]; then
-	echo FAIL: Got error code $? different from expected $err_code for $input
-        fi
-	if [ $err_code = 0 ]; then
-	diff -q $output $expected >> results.log;
-	fi
+	echo Assembling $asm_file >> test.log
+	./assemble $asm_file machine_code.obj >> test.log
+	../lc3bsim2 machine_code.obj < $in_cmd
+	diff -q dumpsim $golden >> results.log;
 done
 if [ -s results.log ]; then
 echo FAIL: results.log is not empty
