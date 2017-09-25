92d0b21c2e8e91d0fbdab008e544f7c71226a0d3
esalcort <32207831+esalcort@users.noreply.github.com>
2017-09-25 17:16:00 -0500

    Finished br_st test
    This tests:
    + BR (for n, z, and p)
    + ADD with negative results
    + sext
    + setCC
    + LEA
    + STB
    + LDW
    + LSHF
    + RSHFA (for positive value)

--- test/br_st.asm
+++ test/br_st.asm
@@ -5,17 +5,18 @@
 	brp positive
 	xor r0, r0, r0 ; r0 = 0
 positive add r3, r2, #-7; 			r3 = -4
-	brp negative
+	brn negative
 	xor r1, r1, r1 ; r1 = 0
 negative add r4, r3, #4 ; 			r4 = 0
 	brz zero
 	xor r2, r2, r2 ; r2 = 0
 zero lshf r2, r2, #10 ; r2 = 3 << 10 = 0x0C00 (1100.0000.0000)
 	add r2, r2, #7 ; r2 = 0x0C07
-	lea r5, testStb ; 				r5 = 0x3050
-	stb r2, r5, #0 ; 				mem[0x3050][0] = 07
+	lea r5, testStb ; 				r5 = 0x3028
+	ldw r6, r5, #0 ;    			r6 = 0x3050
+	stb r2, r6, #0 ; 				mem[0x3050][0] = 07
 	rshfa r2, r2, #8 ; 				r2 = 0x0C
-	stb r2, r5, #1 ;				mem[0x3050][1] = 0x0C
+	stb r2, r6, #1 ;				mem[0x3050][1] = 0x0C
 	halt
 testStb	.fill x3050
 .end
